dddf8105366105e20d06e9e282ba0c1e14261af8
OgiNoegroho <133096668+OgiNoegroho@users.noreply.github.com>
2024-03-22 10:13:47 +0700

    update vscode

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -5,6 +5,7 @@ import React from 'react';
 const About = () => {
 
   return (
+    <>
     <div className="about" style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', backgroundImage: `url('../img/bckgrnd.png')`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
       <main className="content" style={{ padding: '1.4rem 7%', margin: '2em 0', maxWidth: '60rem' }}>
         <h1 style={{ fontSize: '3em', color: '#fff', textShadow: '1px 1px 3px rgba(1, 1, 3, 0.5)' }}>OGYA "OGI" <br /><span>SECIO NOEGROHO.S</span></h1>
@@ -15,6 +16,7 @@ const About = () => {
         <p className="desc" style={{ margin: '0.5rem 16rem 0 0' }}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis nihil quidem, necessitatibus corrupti quasi tempore culpa numquam dolorum sequi explicabo magni neque provident maiores quo dolores, maxime natus commodi accusantium?</p>
       </main>
     </div>
+    </>
   );
 }
 
